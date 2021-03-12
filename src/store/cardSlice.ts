1173f1df6b415b515e91e03ac01cc96eaf4e71b6
seungho <50465901+csh9066@users.noreply.github.com>
2021-03-12 17:07:43 +0900

    Add cardSlice

---
+++ src/store/cardSlice.ts
@@ -0,0 +1,41 @@
+import {
+  createEntityAdapter,
+  createSelector,
+  createSlice,
+} from "@reduxjs/toolkit";
+import { RootState } from ".";
+
+export interface CardState {
+  id: string;
+  title: string;
+  listId: string;
+  order: number;
+}
+
+const cardAdapter = createEntityAdapter<CardState>({
+  selectId: (card) => card.id,
+});
+
+export const cardSlice = createSlice({
+  name: "card",
+  initialState: cardAdapter.getInitialState(),
+  reducers: {
+    addCards: cardAdapter.addMany,
+  },
+});
+
+export const { selectAll: selectAllCards } = cardAdapter.getSelectors(
+  (state: RootState) => state.card
+);
+
+export const selectCardsByListId = createSelector(
+  [selectAllCards, (_: RootState, listId: string) => listId],
+  (cards, listId) =>
+    cards
+      .filter((card) => card.listId === listId)
+      .sort((a, b) => a.order - b.order)
+);
+
+export const { addCards } = cardSlice.actions;
+
+export default cardSlice.reducer;
