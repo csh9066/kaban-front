26fa15b9d6d0344f9341fca252abccca62cba220
seungho <50465901+csh9066@users.noreply.github.com>
2021-03-13 16:16:32 +0900

    Update Button -  color props 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,24 +1,46 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import palette from "../../lib/palette";
 
 interface StyelButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   block?: boolean;
+  color?: "default" | "primary";
 }
 
 interface Props extends StyelButtonProps {
   children?: React.ReactNode;
   icon?: React.ReactNode;
 }
+export default function Button({
+  children,
+  icon,
+  block,
+  color = "default",
+  ...props
+}: Props) {
+  console.log(color);
 
-export default function Button({ children, icon, block, ...props }: Props) {
   return (
-    <StyeldButton block={!!block} {...props}>
+    <StyeldButton block={!!block} color={color} {...props}>
       {icon && <span className="btn__icon">{icon}</span>}
       {children && <span>{children}</span>}
     </StyeldButton>
   );
 }
+
+const colorStyles = {
+  default: css`
+    background-color: hsla(0, 0%, 100%, 0.24);
+    &:hover {
+      background-color: hsla(0, 0%, 100%, 0.32);
+      opacity: 1;
+    }
+  `,
+  primary: css`
+    background-color: ${palette.green};
+  `,
+};
 
 const StyeldButton = styled.button<StyelButtonProps>`
   display: inline-flex;
@@ -28,15 +50,16 @@ const StyeldButton = styled.button<StyelButtonProps>`
   padding: 0 8px;
   border: none;
   border-radius: 3px;
-  background-color: hsla(0, 0%, 100%, 0.24);
   outline: none;
   font-size: 14px;
   color: #fff;
   cursor: pointer;
   appearance: none;
   &:hover {
-    background-color: hsla(0, 0%, 100%, 0.32);
+    opacity: 0.9;
   }
+
+  ${(props) => colorStyles[props.color || "default"]}
 
   .btn__icon {
     display: flex;
