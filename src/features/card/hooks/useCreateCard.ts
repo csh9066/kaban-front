eb8e26f552513c5f320e2fb29be2cc59934ee009
seungho <50465901+csh9066@users.noreply.github.com>
2021-04-17 15:41:27 +0900

    Update useCreateCard

--- src/features/card/hooks/useCreateCard.ts
+++ src/features/card/hooks/useCreateCard.ts
@@ -8,12 +8,12 @@ import {
 } from "../../list/listSlice";
 import { v4 as uuidv4 } from "uuid";
 import { addCard, removeCardById } from "../cardSlice";
+import { CreateCardData } from "../types";
 
 export default function useCreateCard(listId: string) {
   const dispatch = useDispatch();
-  const order =
-    (useSelector((state: RootState) => selectListById(state, listId))?.cards
-      .length as number) + 1;
+  const order = useSelector((state: RootState) => selectListById(state, listId))
+    ?.cards.length as number;
 
   const createCard = async (title: string) => {
     const card = {
@@ -22,9 +22,13 @@ export default function useCreateCard(listId: string) {
     };
     dispatch(addCard(card));
     dispatch(addCardInList({ listId, cardId: card.id }));
-
     try {
-      await api.post("cards", { ...card, order, listId });
+      const data: CreateCardData = {
+        ...card,
+        order,
+        listId,
+      };
+      await api.post("cards", data);
     } catch (e) {
       dispatch(removeCardById(card.id));
       dispatch(removeCardInList({ listId, cardId: card.id }));
--- src/features/card/types.ts
+++ src/features/card/types.ts
@@ -2,3 +2,10 @@ export interface ICard {
   id: string;
   title: string;
 }
+
+export interface CreateCardData {
+  id: string;
+  title: string;
+  order: number;
+  listId: string;
+}
